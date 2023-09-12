53445d27821139d845b549f44083b5f8d3b9ea42
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2023-09-12 13:59:33 -0400

    made so that case ids can only be used once

--- create-new-user/index.js
+++ create-new-user/index.js
@@ -9,63 +9,43 @@ const config = {
   };
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request to create a new user.');
-    let name = req.query.name
-    let caseID = req.query.caseID
-
     let newUser = {
-        name: name,
-        caseID: caseID,
+        firstName: req.query.firstName,
+        lastName: req.query.lastName,
+        caseID: req.query.caseID,
+        birthday: req.query.birthday,
+        password: req.query.password,
       }
 
-    let entries = await createDocument(newUser);
-
-    //const responseMessage = "Your account has been created, " + name
-
-    var jsonArray = JSON.parse(JSON.stringify(entries))
-
+    let status = await createUser(newUser);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: jsonArray
+        body: status
     };
 }
 
-// function to create new database of it does not exist already
-async function create(client, databaseId, containerId) {
-    const partitionKey = config.partitionKey;
-    const { database } = await client.databases.createIfNotExists({
-      id: databaseId
-    });
-    console.log(`Created database:\n${database.id}\n`);
-    const { container } = await client
-      .database(databaseId)
-      .containers.createIfNotExists(
-        { id: containerId, partitionKey },
-        { offerThroughput: 400 }
-      );
-  
-    console.log(`Created container:\n${container.id}\n`);
-  }
-
   // inserting the user data into the database
-async function createDocument(newItem) {
+async function createUser(newUser) {
     const { endpoint, key, databaseId, containerId } = config;
     const client = new CosmosClient({ endpoint, key });
     const database = client.database(databaseId);
     const container = database.container(containerId);
-    await create(client, databaseId, containerId);
-    const { resource: createdItem } = await container.items.create(newItem);
-  
-    // query to return all items
+
+    // query to check if the caseID is already used
     const querySpec = {
-      query: "SELECT * from c"
+        query: `SELECT * from c WHERE c.caseID = "${newUser.caseID}"`
     };
-  
-    // read all items in the items container
-    const { resources: items } = await container.items
-      .query(querySpec)
-      .fetchAll();
-  
-    return items;
+    const { resources: users } = await container.items
+    .query(querySpec)
+    .fetchAll();
+
+    if (users.length == 0){
+        const { resource: createdItem } = await container.items.create(newUser);
+        return "Success."
+    }
+    else {
+        return "CaseID already in use."
+    }
+
   }
