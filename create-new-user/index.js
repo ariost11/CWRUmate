0b7299e9538ff303ec68c6c812410e26ec990fea
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2023-09-14 11:44:15 -0400

    upating create-new-user api

--- create-new-user/index.js
+++ create-new-user/index.js
@@ -21,7 +21,10 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: status
+        body: status,
+        headers: {   
+            'Access-Control-Allow-Credentials': 'true',
+        }
     };
 }
 
