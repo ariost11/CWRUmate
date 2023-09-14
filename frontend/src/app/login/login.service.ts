65212bfd66053730b949f0c3cb1e0bf85fc2d59f
Regentmango7 <regentmango7@gmail.com>
2023-09-14 12:56:50 -0400

    Marginal Changes

--- frontend/src/app/login/login.service.ts
+++ frontend/src/app/login/login.service.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+var url = `https://cwrumate.azurewebsites.net/api/create-new-user/`
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +11,18 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   addUser(firstName: string, lastName: string, caseID: string, birthday: string, password: string): Observable<any>{
-    return this.http.get(`https://cwrumate.azurewebsites.net/api/create-new-user?firstName=${firstName}&lastName=${lastName}&caseID=${caseID}&birthday=${birthday}&password=${password}`);
+    return this.http.get(url,
+      {
+        headers: {
+          
+        },
+        params: {
+          firstName: firstName,
+          lastName: lastName,
+          caseID: caseID,
+          birthday: birthday,
+          password: password
+        }
+      });
   }
 }
