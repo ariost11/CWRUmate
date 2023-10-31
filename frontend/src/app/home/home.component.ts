6366eff2998d2884d511d99a18919d887a379dda
Regentmango7 <regentmango7@gmail.com>
2023-10-31 13:24:40 -0400

    Trying... Crying... :cry:

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -5,6 +5,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styles: [`
+    :host {
+      width: 100vw;
+    }`],
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
