c6472f54dc86f4784de12274552aeb86ba328d7b
Regentmango7 <regentmango7@gmail.com>
2023-12-03 16:10:18 -0500

    add single quote fix

--- frontend/src/app/shared/modal/modal.component.ts
+++ frontend/src/app/shared/modal/modal.component.ts
@@ -118,6 +118,6 @@ export class ModalComponent implements OnInit {
     }
 
 	replaceSpace(str: string){
-		return str.replaceAll(" ", "_").replaceAll('"', "_");
+		return str.replaceAll(" ", "_").replaceAll('"', "_").replaceAll("'", "_");
 	}
 }
