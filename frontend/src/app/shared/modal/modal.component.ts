bd555972832ffc4d74263c4d9a82d957569c1302
ariost11 <ariostrow@gmail.com>
2023-12-02 19:38:13 -0500

    MESSAGES WORKSSSSSS

--- frontend/src/app/shared/modal/modal.component.ts
+++ frontend/src/app/shared/modal/modal.component.ts
@@ -26,10 +26,6 @@ export class ModalComponent implements OnInit {
 	messages: any[];
 	myName = 'ERROR';
 
-	now(){
-		return new Date();
-	}
-
 	activateProfileCard(){
 		this.profile_display = true;
 		this.modalLoading = true;
@@ -39,42 +35,46 @@ export class ModalComponent implements OnInit {
 		}, () => {}, () => {this.modalLoading = false;});
 	}
 
+	getCount() {
+		return this.messages[this.messages.length - 1].count;
+	}
+
 	sendMessage($event: { message: string; }) {
 		var newMessage = {
 			text: $event.message,
 			user: 'true',
 			name: this.myName,
 			date: new Date(),
-			count: this.modalService.getCount() + 1,
+			count: this.getCount() + 1,
 		}
 
-		this.modalService.sendMessage(newMessage.text, newMessage.date.toString(), this.caseID, this.otherID, newMessage.count).subscribe(resp => {
+		this.modalService.sendMessage(newMessage.text, newMessage.date.toString(), this.caseID, this.otherID).subscribe(resp => {
 			if(resp.resp === 'SUCCESS')
 				this.messages.push(newMessage);
 		});
 	}
 
 	getAllMessages() {
+		console.log('getting all messages');
 		this.modalLoading = true;
 		this.modalService.getRecentMessages(this.caseID, this.otherID, 0).subscribe(resp => {
-			if(resp.resp === 'SUCCESS') {
-				this.messages = resp.resp.messages;
-			}
+			resp.resp.forEach((a : any) => {
+				this.messages.push(a);
+			});
 		}, () => {}, () => {
 			this.modalLoading = false;
 		});
 	}
 
 	addRecentMessages() {
-		this.modalLoading = true;
-		this.modalService.getRecentMessages(this.caseID, this.otherID, this.modalService.getCount()).subscribe(resp => {
-			if(resp.resp === 'SUCCESS') {
-				resp.resp.messages.forEach((message: any) => {
-					this.messages.push(message);
-				});
-			}
+		//this.modalLoading = true;
+		this.modalService.getRecentMessages(this.caseID, this.otherID, this.getCount()).subscribe(resp => {
+			resp.resp.forEach((a: any) => {
+				if(!this.messages.includes(a))
+					this.messages.push(a);
+			});
 		}, () => {}, () => {
-			this.modalLoading = false;
+			//this.modalLoading = false;
 		});
 	}
 
@@ -87,20 +87,27 @@ export class ModalComponent implements OnInit {
 		//loop messages check
 		var tempName = this.name;
 		var modalOpen = false;
-		var profile_display = this.profile_display;
-		document.addEventListener("DOMContentLoaded", () => {
-			var modal = document.getElementById(`chatModal_${tempName}`);
-			modal?.addEventListener('hidden.bs.modal', function (event) {
-				modalOpen = false;
-			});
-			modal?.addEventListener('shown.bs.modal', function (event) {
-				modalOpen = true;
-			});
-			setInterval(() =>{
-				if(modalOpen && !this.profile_display)
-					console.log('open');
-					//this.addRecentMessages();
-			}, 1000);
+		var firstClicked = true;
+		document.addEventListener('click', () => {
+			if(firstClicked) {
+				var modal = document.getElementById(`chatModal_${tempName}`);
+				console.log(modal);
+				modal?.addEventListener('hidden.bs.modal', () => {
+					modalOpen = false;
+				});
+				modal?.addEventListener('shown.bs.modal', () => {
+					if(this.messages.length === 0)
+						this.getAllMessages();
+					modalOpen = true;
+				});
+
+				setInterval(() => {
+					if(modalOpen && !this.profile_display) {
+						this.addRecentMessages();
+					}
+				}, 1000);
+			}
+			firstClicked = false;
 		});
 	}
 }
--- frontend/src/app/shared/modal/modal.service.ts
+++ frontend/src/app/shared/modal/modal.service.ts
@@ -14,64 +14,8 @@ export class ModalService {
 
 	constructor(private http: HttpClient) { }
 
-	messages = [
-		{
-			text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
-			reply: false,
-			date: new Date(),
-			name: 'Amy',
-			count: 0,
-		},
-		{
-			text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
-			reply: true,
-			date: new Date(),
-			name: 'Ari',
-			count: 1,
-		},
-		{
-			text: 'Hello, how are you?',
-			reply: false,
-			date: new Date(),
-			name: 'Amy',
-			count: 2,
-		},
-		{
-			text: 'Hey looks at that pic I just found!',
-			reply: true,
-			date: new Date(),
-			name: 'Ari',
-			count: 3,
-		},
-		{
-			text: 'What do you mean by that?',
-			reply: false,
-			date: new Date(),
-			name: 'Amy',
-			count: 4,
-		},
-		{
-			text: 'Attached is an archive I mentioned',
-			reply: false,
-			date: new Date(),
-			name: 'Amy',
-			count: 5,
-		},
-		{
-			text: 'Meet me there',
-			reply: true,
-			date: new Date(),
-			name: 'Ari',
-			count: 6,
-		},
-	];
-
 	loadMessages() {
 		return [];
-	}
-
-	getCount() {
-		return this.messages[this.messages.length - 1].count;
 	}
 
 	getName(caseID: string): Observable<any> {
@@ -82,14 +26,13 @@ export class ModalService {
 		});
 	}
 
-	sendMessage(text: string, date: string, senderID: string, recieverID: string, count: number): Observable<any> {
+	sendMessage(text: string, date: string, senderID: string, recieverID: string): Observable<any> {
 		return this.http.get(sendMessage, {
 			params: {
 				text: text,
 				date: date,
-				senderID: senderID,
-				recieverID: recieverID,
-				count: count,
+				userA: senderID,
+				userB: recieverID,
 			}
 		});
 	}
